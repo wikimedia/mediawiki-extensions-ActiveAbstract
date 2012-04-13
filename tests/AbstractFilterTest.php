812bf79ae22291b046d337670d47d39b6125e3c7
Christian Aistleitner <christian@quelltextlich.at>
2012-04-13 18:29:46 +0200

    Adding tests for AbstractFilter
    Change-Id: I1e62e966188f077a274a0bc83f4b30c76e9e5a77

---
+++ tests/AbstractFilterTest.php
@@ -0,0 +1,357 @@
+<?php
+require_once( dirname( __FILE__ ) . "/../AbstractFilter.php" );
+
+
+
+/**
+ * An Stub implementation taking return values from an array while removing
+ * used parameter/return-value entries therefrom.
+ *
+ * This stub overcomes limitations of using once() multiple times for different
+ * parameter values, while not caring about the invocation order.
+ *
+ * To get the not-working PHPUnit mock code
+ *
+ *   mock->expects( $this->once() )
+ *       ->method( 'methodName' )
+ *       ->parameters( $this->equalTo( param1 ), $this->equalTo( param2 ) )
+ *       ->will( $this->returnValue( ret12 ) );
+ *   mock->expects( $this->once() )
+ *       ->method( 'methodName' )
+ *       ->parameters( $this->equalTo( param3 ), $this->equalTo( param4 ) )
+ *       ->will( $this->returnValue( ret34 ) );
+ *
+ * (the above code ignore the first of the two statements) to do what it is
+ * supposed to do (i.e.: asserting that methodName is called exactly two times.
+ * Once with parameters param1, param2 returning ret12, and once with
+ * parameters param3, param4 returning ret34 (which of the two happens first
+ * does not matter), you can use the following code:
+ *
+ *   $map = array(
+ *     array( param1, param2, ret12),
+ *     array( param3, param4, ret34)
+ *   );
+ *   mock->expects( $this->exactly( count( $map ) ) )
+ *       ->method( 'methodName' )
+ *       ->will( new DroppingReturnValueMap( $map ) );
+ *
+ * This code asserts that each parameter given in $map is used exactly once,
+ * while each call returns the given return value.
+ *
+ *
+ * The implementation of DroppingReturnValueMap is based off of
+ * PHPUnit_Framework_MockObject_Stub_ReturnValueMap from PHPUnit_MockObject by
+ * Sebastian Bergmann (under the 3-clause-BSD licence).
+ */
+class DroppingReturnValueMap implements PHPUnit_Framework_MockObject_Stub
+{
+	protected $valueMap;
+
+	public function __construct( array $valueMap )
+	{
+		$this->valueMap = $valueMap;
+	}
+
+	public function invoke( PHPUnit_Framework_MockObject_Invocation $invocation )
+	{
+		$parameterCount = count( $invocation->parameters );
+
+		foreach ( $this->valueMap as $key => $map ) {
+			if ( !is_array( $map ) || $parameterCount != count( $map ) - 1 ) {
+				continue;
+			}
+
+			$return = array_pop( $map );
+			if ( $invocation->parameters === $map ) {
+				unset( $this->valueMap[$key] );
+				return $return;
+			}
+		}
+
+		// Could not find the actual parameters in valueMap. We signal failure, after
+		// formatting the actual parameters in $actual, to have a nice error message
+		$actual = "(";
+		$connective = "";
+		foreach ( $invocation->parameters as $parameter ) {
+			$actual .= $connective . " ";
+			try {
+				$actual .= strval( $parameter );
+			} catch ( Exception $e ) {
+				$actual .= "*";
+			}
+			$connective = ",";
+		}
+		if ( $parameterCount > 0 ) {
+			$actual .= " ";
+		}
+		$actual .= ")";
+
+		throw new PHPUnit_Framework_ExpectationFailedException( "Map for DroppingReturnValueMap "
+			. "does not (or no longer) hold an entry for the actual parameters $actual" );
+	}
+
+	public function toString()
+	{
+		return 'dropping return value from a map';
+	}
+}
+
+
+
+/**
+ * Unit tests for Abstractfilter
+ *
+ * Tests for AbstractFilter::writeOpenPage and AbstractFilter::writeClosePage
+ * (and in depth tests of AbstractFilter::writeRevision) are missing, as their
+ * working relies on volatile database internals. We test them in the
+ * integration tests within BackupDumperAbstractsTest.
+ *
+ * @group Database
+ * @group Dump
+ */
+class AbstractFilterTest extends DumpTestCase {
+
+	/**
+	 * obtains a stub implementation that allows to map parameters to values
+	 * and retract the parameters after usage.
+	 *
+	 * @param $valueMap array Mapping between parameters and return values. The
+	 *              documentation of DroppingReturnValueMap provides an example.
+	 * @return DroppingReturnValueMap
+	 *
+	 * @see DroppingReturnValueMap
+	 */
+	function droppingReturnValueMap( $valueMap ) {
+		return new DroppingReturnValueMap( $valueMap );
+	}
+
+	function testRegister() {
+		$map = array(
+			array( 'abstract', 'AbstractFilter', NULL ),
+			array( 'noredirect', 'NoredirectFilter', NULL )
+		);
+
+		$dumperMock = $this->getMock( 'BackupDumper', array(), array(), '', FALSE );
+
+		$dumperMock->expects( $this->exactly( count( $map ) ) )
+			->method( 'registerFilter' )
+			->will( $this->droppingReturnValueMap( $map ) );
+
+		AbstractFilter::register( $dumperMock );
+
+	}
+
+	function testWriteOpenStreamNull() {
+		$sinkMock = $this->getMock( 'DumpOutput' );
+
+		$sinkMock->expects( $this->exactly( 1 ) )
+			->method( 'writeOpenStream' )
+			->with( $this->matchesRegularExpression( '@^[[:space:]]*<feed>[[:space:]]*$@' ) );
+
+		// Checking against side effects
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeCloseStream' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenPage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeClosePage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeRevision' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeLogItem' );
+
+		// Performing the actual test
+
+		$af = new AbstractFilter( $sinkMock );
+		$af->writeOpenStream( null );
+	}
+
+	function testWriteOpenStreamEmptyString() {
+		$sinkMock = $this->getMock( 'DumpOutput' );
+
+		$sinkMock->expects( $this->exactly( 1 ) )
+			->method( 'writeOpenStream' )
+			->with( $this->matchesRegularExpression( '@^[[:space:]]*<feed>[[:space:]]*$@' ) );
+
+		// Checking against side effects
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeCloseStream' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenPage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeClosePage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeRevision' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeLogItem' );
+
+		// Performing the actual test
+
+		$af = new AbstractFilter( $sinkMock );
+		$af->writeOpenStream( "" );
+	}
+
+	function testWriteOpenStreamText() {
+		$sinkMock = $this->getMock( 'DumpOutput' );
+
+		$sinkMock->expects( $this->exactly( 1 ) )
+			->method( 'writeOpenStream' )
+			->with( $this->matchesRegularExpression( '@^[[:space:]]*<feed>[[:space:]]*$@' ) );
+
+		// Checking against side effects
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeCloseStream' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenPage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeClosePage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeRevision' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeLogItem' );
+
+		// Performing the actual test
+
+		$af = new AbstractFilter( $sinkMock );
+		$af->writeOpenStream( "foo" );
+	}
+
+	function testWriteCloseStreamNull() {
+		$sinkMock = $this->getMock( 'DumpOutput' );
+
+		$sinkMock->expects( $this->exactly( 1 ) )
+			->method( 'writeCloseStream' )
+			->with( $this->matchesRegularExpression( '@^[[:space:]]*</feed>[[:space:]]*$@' ) );
+
+		// Checking against side effects
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenStream' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenPage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeClosePage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeRevision' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeLogItem' );
+
+		// Performing the actual test
+
+		$af = new AbstractFilter( $sinkMock );
+		$af->writeCloseStream( null );
+	}
+
+	function testWriteCloseStreamEmptyString() {
+		$sinkMock = $this->getMock( 'DumpOutput' );
+
+		$sinkMock->expects( $this->exactly( 1 ) )
+			->method( 'writeCloseStream' )
+			->with( $this->matchesRegularExpression( '@^[[:space:]]*</feed>[[:space:]]*$@' ) );
+
+		// Checking against side effects
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenStream' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenPage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeClosePage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeRevision' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeLogItem' );
+
+		// Performing the actual test
+
+		$af = new AbstractFilter( $sinkMock );
+		$af->writeCloseStream( "" );
+	}
+
+	function testWriteCloseStreamText() {
+		$sinkMock = $this->getMock( 'DumpOutput' );
+
+		$sinkMock->expects( $this->exactly( 1 ) )
+			->method( 'writeCloseStream' )
+			->with( $this->matchesRegularExpression( '@^[[:space:]]*</feed>[[:space:]]*$@' ) );
+
+		// Checking against side effects
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenStream' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenPage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeClosePage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeRevision' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeLogItem' );
+
+		// Performing the actual test
+
+		$af = new AbstractFilter( $sinkMock );
+		$af->writeCloseStream( "foo" );
+
+	}
+
+	function testWriteRevision() {
+		$sinkMock = $this->getMock( 'DumpOutput' );
+
+		// No output of any kind is expected, as the filter outputs only the most current
+		// revision, and can detect the most recent only only after all revisions have been
+		// passed.
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenStream' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeCloseStream' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeOpenPage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeClosePage' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeRevision' );
+
+		$sinkMock->expects( $this->never() )
+			->method( 'writeLogItem' );
+
+		// Performing the actual test
+
+		$af = new AbstractFilter( $sinkMock );
+		$af->writeRevision( "foo", "bar" );
+
+	}
+
+}
---
+++ tests/backup_AbstractTest.php
@@ -0,0 +1,387 @@
+<?php
+require_once( dirname( __FILE__ ) . "/../AbstractFilter.php" );
+
+
+
+/**
+ * Tests for BackupDumper producing abstract dumps using Abstractfilter
+ *
+ * This test case is not strictlly a unit test, but a crucial integration
+ * test for our dump infrastructure
+ *
+ * @group medium
+ * @group Database
+ * @group Dump
+ */
+class BackupDumperAbstractsTest extends DumpTestCase {
+
+	// We'll add several pages, revision and texts. The following variables hold the
+	// corresponding ids.
+	private $pageId1, $pageId2, $pageId3, $pageId4, $pageId5, $pageId6, $pageId7;
+	private $revId1_1, $textId1_1;
+	private $revId2_1, $textId2_1, $revId2_2, $textId2_2;
+	private $revId3_1, $textId3_1, $revId3_2, $textId3_2;
+	private $revId4_1, $textId4_1;
+	private $revId5_1, $textId5_1;
+	private $revId6_1, $textId6_1;
+	private $revId7_1, $textId7_1;
+
+	function addDBData() {
+		$this->tablesUsed[] = 'page';
+		$this->tablesUsed[] = 'revision';
+		$this->tablesUsed[] = 'text';
+
+		try {
+			// Simple Page. With a section, to not trigger category checking,
+			// that requires a second connection to the same database (compare
+			// Page 6 below)
+			$title = Title::newFromText( 'BackupDumperAbstractsTestPage1' );
+			$page = WikiPage::factory( $title );
+			list( $this->revId1_1, $this->textId1_1 ) = $this->addRevision( $page,
+				"BackupDumperAbstractsTestPage1Text1
+
+=Subsection 1=",
+				"BackupDumperAbstractsTestPage1Summary1" );
+			$this->pageId1 = $page->getId();
+
+			// Page with more than one revision and more subsections
+			$title = Title::newFromText( 'BackupDumperAbstractsTestPage2' );
+			$page = WikiPage::factory( $title );
+			list( $this->revId2_1, $this->textId2_1 ) = $this->addRevision( $page,
+				"BackupDumperAbstractsTestPage2Text1",
+				"BackupDumperAbstractsTestPage2Summary1" );
+			 list( $this->revId2_2, $this->textId2_2 ) = $this->addRevision( $page,
+				"A short first paragraph.
+
+A second paragraph.
+
+
+=Subsection 1=
+Short paragraph in subsection 1.
+
+Second paragraph in subsection 1.
+
+=Subsection 2=
+Short paragraph in subsection 2.
+
+Second paragraph in subsection 2.
+
+==Subsection 2.1==
+Short paragraph in subsection 2.1.
+
+Second paragraph in subsection 2.1.
+
+===Subsection 2.1.1===
+Short paragraph in subsection 2.1.1.
+
+Second paragraph in subsection 2.1.1.
+
+====Subsection 2.1.1.1====
+Short paragraph in subsection 2.1.1.1.
+
+Second paragraph in subsection 2.1.1.1.
+
+==Final & Last subsection==
+",
+				"BackupDumperAbstractsTestPage1Summary1" );
+			$this->pageId2 = $page->getId();
+
+			// Deleted Page
+			$title = Title::newFromText( 'BackupDumperAbstractsTestPage3' );
+			$page = WikiPage::factory( $title );
+			list( $this->revId3_1, $this->textId3_1 ) = $this->addRevision( $page,
+				"BackupDumperAbstractsTestPage3Text1", "BackupDumperAbstractsTestPage2Summary1" );
+			list( $this->revId3_2, $this->textId3_2 ) = $this->addRevision( $page,
+				"BackupDumperAbstractsTestPage3Text2", "BackupDumperAbstractsTestPage2Summary2" );
+			$this->pageId3 = $page->getId();
+			$page->doDeleteArticle( "Testing ;)" );
+
+			// Page in different namespace
+			$title = Title::newFromText( 'BackupDumperAbstractsTestPage1', NS_TALK );
+			$page = WikiPage::factory( $title );
+			list( $this->revId4_1, $this->textId4_1 ) = $this->addRevision( $page,
+				"Talk about BackupDumperAbstractsTestPage1 Text1
+
+Second paragraph",
+				"Talk BackupDumperAbstractsTestPage1 Summary1" );
+			$this->pageId4 = $page->getId();
+
+			// Redirecting page
+			$title = Title::newFromText( 'BackupDumperAbstractsTestPage5' );
+			$page = WikiPage::factory( $title );
+			list( $this->revId5_1, $this->textId5_1 ) = $this->addRevision( $page,
+				"#REDIRECT [[Page1]]",
+				"BackupDumperAbstractsTestPage5Summary1" );
+			$this->pageId5 = $page->getId();
+
+			// Page without subsections
+			$title = Title::newFromText( 'BackupDumperAbstractsTestPage6' );
+			$page = WikiPage::factory( $title );
+			list( $this->revId6_1, $this->textId6_1 ) = $this->addRevision( $page,
+				"BackupDumperAbstractsTestPage6Text1",
+				"BackupDumperAbstractsTestPage6Summary1" );
+			$this->pageId6 = $page->getId();
+
+			// Page with category links
+			$title = Title::newFromText( 'BackupDumperAbstractsTestPage7' );
+			$page = WikiPage::factory( $title );
+			list( $this->revId7_1, $this->textId7_1 ) = $this->addRevision( $page,
+				"BackupDumperAbstractsTestPage7Text1
+
+Link to Page1 as Category [[Category:BackupDumperAbstractsTestPage1]].
+Link to Page2 as Page [[BackupDumperAbstractsTestPage2]].
+Link to Page7 as Category [[Category:BackupDumperAbstractsTestPage7]].
+",
+				"BackupDumperAbstractsTestPage7Summary1" );
+			$this->pageId7 = $page->getId();
+
+
+
+		} catch ( Exception $e ) {
+			// We'd love to pass $e directly. However, ... see
+			// documentation of exceptionFromAddDBData in
+			// DumpTestCase
+			$this->exceptionFromAddDBData = $e;
+		}
+
+	}
+
+	function setUp() {
+		parent::setUp();
+
+		// Since we will restrict dumping by page ranges (to allow
+		// working tests, even if the db gets prepopulated by a base
+		// class), we have to assert, that the page id are consecutively
+		// increasing
+		$this->assertEquals(
+			array( $this->pageId2, $this->pageId3, $this->pageId4,
+				$this->pageId5, $this->pageId6, $this->pageId7 ),
+			array( $this->pageId1 + 1, $this->pageId2 + 1, $this->pageId3 + 1,
+				$this->pageId4 + 1, $this->pageId5 + 1, $this->pageId6 + 1 ),
+			"Page ids increasing without holes" );
+
+	}
+
+	/**
+	 * Opens an XML file to analyze as feed.
+	 *
+	 * A opening feed tag is asserted and skipped over.
+	 *
+	 * @param $fname string: name of file to analyze
+	 */
+	private function assertFeedStart( $fname ) {
+		$this->assertDumpStart( $fname, false );
+		$this->xml->read();
+		$this->assertNodeStart( "feed" );
+		$this->skipWhitespace();
+	}
+
+	/**
+	 * Asserts that the xml reader is at the final closing tag of the feed file and
+	 * closes the reader.
+	 */
+	private function assertFeedEnd() {
+		$this->assertDumpEnd( "feed" );
+	}
+
+	/**
+	 * Asserts that the xml reader is at the start of a doc element and skips over the
+	 * first tags, after checking them.
+	 *
+	 * Besides the opening doc element, this function also checks for and skips over the
+	 * title, url, and abstract tags and also the opening links element. Hence, after
+	 * this function, the xml reader is at the first link element of the doc.
+	 *
+	 * @param $title string: title of the doc
+	 * @param $abstract string: abstract of the doc
+	 * @param $ns int: (optional) namespace for the title
+	 */
+	private function assertDocStart( $title, $abstract, $ns = NS_MAIN ) {
+		global $wgSitename;
+		$this->assertNodeStart( "doc" );
+		$this->skipWhitespace();
+
+		$title = Title::makeTitle( $ns, $title );
+		$this->assertNotNull( $title, "Title generation for <doc> tag" );
+		$this->assertTextNode( "title", $wgSitename . ": " . $title->getPrefixedText() );
+
+		$this->currentDocURL = $title->getCanonicalURL();
+		$this->assertTextNode( "url", $this->currentDocURL );
+
+		$this->assertTextNode( "abstract", $abstract );
+
+		$this->assertNodeStart( "links" );
+		$this->skipWhitespace();
+	}
+
+	/**
+	 * Asserts that the xml reader is after the last link element of the doc.
+	 *
+	 * The function skips past closing links and doc elements.
+	 */
+	private function assertDocEnd() {
+		$this->assertNodeEnd( "links" );
+		$this->skipWhitespace();
+
+		$this->assertNodeEnd( "doc" );
+		$this->skipWhitespace();
+	}
+
+	/**
+	 * Asserts that the xml reader is at a link element and skips over it, while analyzing it.
+	 *
+	 * @param $name string: name of the link
+	 * @param $is_category bool: (optional) Whether or not the link is a link to a category.
+	 *             If true, $name is the categories title without namespace. If false, $name
+	 *             is interpreted as name of a subsection within the current doc.
+	 */
+	private function assertLink( $name, $is_category = false ) {
+		global $wgContLang;
+		$this->assertNodeStart( "sublink" );
+		$this->skipWhitespace();
+
+		$this->assertTextNode( "anchor", $name );
+		if ( $is_category ) {
+			$link = Title::makeTitle( NS_CATEGORY, $name );
+			$this->assertTextNode( "link", $link->getCanonicalURL() );
+		} else {
+			$this->assertTextNode( "link", $this->currentDocURL . "#"
+				. str_replace( array( ' ', '&' ), array( '_', '.26' ), $name ) );
+		}
+
+		$this->assertNodeEnd( "sublink" );
+		$this->skipWhitespace();
+	}
+
+	function testPlain () {
+		global $IP;
+
+		// Setting up the dump
+		$fname = $this->getNewTempFile();
+		$dumper = new BackupDumper( array(
+				"--plugin=AbstractFilter:$IP/extensions/ActiveAbstract/AbstractFilter.php",
+				"--output=file:" . $fname, "--filter=abstract" ) );
+		$dumper->startId = $this->pageId1;
+		$dumper->endId = $this->pageId4 + 1; // Not including the redirect page (db isolation)
+		$dumper->reporting = false;
+		$dumper->setDb( $this->db );
+
+		// Performing the dump
+		$dumper->dump( WikiExporter::FULL, WikiExporter::TEXT );
+
+		// Checking results
+		$this->assertFeedStart( $fname );
+
+		$this->assertDocStart( "BackupDumperAbstractsTestPage1",
+			"BackupDumperAbstractsTestPage1Text1" );
+		$this->assertLink( "Subsection 1" );
+		$this->assertDocEnd();
+
+		// --current is not added in $dumper's constructor. Nevertheless,
+		// only the current revision is dumped. This happens on purpose,
+		// as documented in AbstractFilter
+		$this->assertDocStart( "BackupDumperAbstractsTestPage2",
+			"A short first paragraph." );
+		$this->assertLink( "Subsection 1" );
+		$this->assertLink( "Subsection 2" );
+		$this->assertLink( "Subsection 2.1" );
+		$this->assertLink( "Subsection 2.1.1" );
+		$this->assertLink( "Subsection 2.1.1.1" );
+		$this->assertLink( "Final & Last subsection" );
+		$this->assertDocEnd();
+
+		// Page 3 is deleted, hence not visible
+
+		$this->assertDocStart( "BackupDumperAbstractsTestPage1",
+			"Talk about BackupDumperAbstractsTestPage1 Text1", NS_TALK );
+		$this->assertDocEnd();
+
+		$this->assertFeedEnd();
+
+	}
+
+	function testXmlDumpsBackupUseCase () {
+		global $IP;
+
+		// When dumping pages that contain no subsections (this is what we will to with
+		// pages 6, and 7), AbstractFilter tries to check for the pages' categories to
+		// use as links. Therefore, AbstractFilter grabs a new database connection,
+		// hence does not see the temporary tables created by the test suite. We cannot
+		// add and use dependency injection in AbstractFilter to overcome this, as this
+		// test's database connection is right in the middle of yielding the (unbuffered)
+		// result of querying for the pages/revisions.
+		//
+		// We could of course add means to force buffered resultsets for the dump process,
+		// but this would no longer represent xmldumps-backups use case.
+		//
+		// Long story short: When using temporary tables, we have to skip the test :(
+		if ( $this->useTemporaryTables ) {
+			$this->markTestSkipped( "This test grabs new database connections at "
+				. "several times. Run the test suite with --use-normal-tables "
+				. "to not skip this test" );
+		}
+
+		// Setting up the dump
+		$fname = $this->getNewTempFile();
+		$dumper = new BackupDumper( array(
+				"--plugin=AbstractFilter:$IP/extensions/ActiveAbstract/AbstractFilter.php",
+				"--current", "--output=file:" . $fname,  "--filter=namespace:NS_MAIN",
+				"--filter=noredirect", "--filter=abstract" ) );
+		$dumper->startId = $this->pageId1;
+		$dumper->endId = $this->pageId7 + 1;
+
+		// xmldumps-backup uses reporting. We will not check the exact reported
+		// message, as they are dependent on the processing power of the used
+		// computer. We only check that reporting does not crash the dumping
+		// and that something is reported
+		$dumper->stderr = fopen( 'php://output', 'a' );
+		if ( $dumper->stderr === FALSE ) {
+			$this->fail( "Could not open stream for stderr" );
+		}
+
+		// Performing the dump
+		$dumper->dump( WikiExporter::FULL, WikiExporter::TEXT );
+
+		$this->assertTrue( fclose( $dumper->stderr ), "Closing stderr handle" );
+
+		// Checking results
+		$this->assertFeedStart( $fname );
+
+		$this->assertDocStart( "BackupDumperAbstractsTestPage1",
+			"BackupDumperAbstractsTestPage1Text1" );
+		$this->assertLink( "Subsection 1" );
+		$this->assertDocEnd();
+
+		// Only the current revision of this page is vilible
+		$this->assertDocStart( "BackupDumperAbstractsTestPage2",
+			"A short first paragraph." );
+		$this->assertLink( "Subsection 1" );
+		$this->assertLink( "Subsection 2" );
+		$this->assertLink( "Subsection 2.1" );
+		$this->assertLink( "Subsection 2.1.1" );
+		$this->assertLink( "Subsection 2.1.1.1" );
+		$this->assertLink( "Final & Last subsection" );
+		$this->assertDocEnd();
+
+		// Page 3 is deleted, hence not visible
+
+		// Page 4 is NS_TALK, hence not visible
+
+		// Page 5 is a redirect, hence not visible
+
+		$this->assertDocStart( "BackupDumperAbstractsTestPage6",
+			"BackupDumperAbstractsTestPage6Text1" );
+		$this->assertDocEnd();
+
+		$this->assertDocStart( "BackupDumperAbstractsTestPage7",
+			"BackupDumperAbstractsTestPage7Text1" );
+		$this->assertLink( "BackupDumperAbstractsTestPage1", true );
+		$this->assertLink( "BackupDumperAbstractsTestPage7", true );
+		$this->assertDocEnd();
+
+		$this->assertFeedEnd();
+
+		$this->expectOutputRegex( '/page.*, [0-9]* revs .*, ETA/' );
+	}
+
+}
