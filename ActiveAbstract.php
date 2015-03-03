180792b6d70bd92db48bd8b34153d02afaf9440c
Kunal Mehta <legoktm@gmail.com>
2015-03-02 16:40:42 -0800

    Add extension.json, empty PHP entry point
    Bug: T87880
    Change-Id: I3af2ff032c4f3bc793e2e4f06723b83f76a3944d

--- ActiveAbstract.php
+++ ActiveAbstract.php
@@ -1,28 +1,11 @@
 <?php
-/*
- (c) Aaron Schulz, Joerg Baach, 2007-2008 GPL
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along
- with this program; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- http://www.gnu.org/copyleft/gpl.html
-*/
-
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo "ActiveAbstract extension\n";
-	exit( 1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'ActiveAbstract' );
+	/* wfWarn(
+		'Deprecated PHP entry point used for ActiveAbstract extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the ActiveAbstract extension requires MediaWiki 1.25+' );
 }
-
-$wgAutoloadClasses['ActiveAbstractTestHooks'] = ( dirname( __FILE__ ) . "/tests/ActiveAbstractTest.hooks.php" );
-
-$wgHooks['UnitTestsList'][] = 'ActiveAbstractTestHooks::getUnitTests';
