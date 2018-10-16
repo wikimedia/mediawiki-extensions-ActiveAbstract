241cba67832328df7626d6f9d192464b241e8111
Kunal Mehta <legoktm@member.fsf.org>
2018-10-16 11:24:52 -0700

    Warn if legacy PHP entry point is used
    Bug: T140851
    Change-Id: I37a1a041e07e5cf03edfb637d9fc8b97a0cd378c

--- ActiveAbstract.php
+++ ActiveAbstract.php
@@ -3,11 +3,11 @@
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'ActiveAbstract' );
 
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for ActiveAbstract extension. ' .
 		'Please use wfLoadExtension instead, see ' .
 		'https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 
 	return;
 } else {
